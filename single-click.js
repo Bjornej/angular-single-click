11760ca5d3cca5f2470a44920ae62c4afb916444
Paolo Nicodemo <paolonicodemo@gmail.com>
2014-05-03 23:09:04 +0200

    Completed binding and added test page

--- single-click.js
+++ single-click.js
@@ -1,15 +1,27 @@
-app.directive("ngSingleClick",function(){
-	return {
-		restrict : "A",
-        compile: function($element, attr) {
-          var fn = $parse(attr.ngSingleClick);
-          return function(scope, element, attr) {
-            element.on("click", function(event) {
-              scope.$apply(function() {
-                fn(scope, {$event:event});
-              });
-            });
-          };
-		}
-	};
-});
+angular.module("ng").directive("ngSingleClick", ["$parse", function ($parse) {
+    return {
+        restrict: "A",
+        controller: ['$scope', function ($scope) {
+            this.running = false;
+        }],
+        compile: function ($element, attr) {
+            var fn = $parse(attr.ngSingleClick);
+            return function (scope, element, attr, controller) {
+                element.on("click", function (event) {
+                    debugger;
+                    if (!controller.running) {
+                        scope.$apply(function () {
+                            var result = fn(scope, { $event: event });
+                            if(result.finally!==undefined){
+                                controller.running=true;
+                                result.finally(function(){
+                                    controller.running=false;
+                                });
+                            }
+                        });
+                    }
+                });
+            };
+        }
+    };
+} ]);
