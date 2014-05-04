3a6f836091917e3282c7f1c84127dc000446e53b
Paolo Nicodemo <paolonicodemo@gmail.com>
2014-05-04 17:01:47 +0200

    Refactoring, modified gitignore and added grunt

---
+++ README.md
@@ -0,0 +1,10 @@
+### Angular-single-click
+
+Simple angular directive used to prevent double clicks.
+
+
+What this directive do is to invoke the specified function on click, like ng-click, but it inspects the returned value to verify if it's a promise.
+
+If the returned value is a promise it disables further clicks until the promise execution ends.
+
+This is useful when a click triggers an ajax call and you want to prevent double requests which are fairly common. All it needs is to remmeber to return the promise from the invoked function.
--- single-click.js
+++ single-click.js
@@ -1,21 +1,26 @@
 angular.module("ng").directive("ngSingleClick", ["$parse", function ($parse) {
+    "use strict";
     return {
         restrict: "A",
         controller: ['$scope', function ($scope) {
+            // indicates if function invoked by the click is currently running
             this.running = false;
         }],
         compile: function ($element, attr) {
             var fn = $parse(attr.ngSingleClick);
             return function (scope, element, attr, controller) {
                 element.on("click", function (event) {
-                    debugger;
+
+                    // if function is currently running don't execute it again
                     if (!controller.running) {
                         scope.$apply(function () {
+
+                            // if invoked function returns a promise wait for it's completion
                             var result = fn(scope, { $event: event });
-                            if(result.finally!==undefined){
-                                controller.running=true;
-                                result.finally(function(){
-                                    controller.running=false;
+                            if (result.finally !== undefined) {
+                                controller.running = true;
+                                result.finally(function () {
+                                    controller.running = false;
                                 });
                             }
                         });
@@ -24,4 +29,4 @@ angular.module("ng").directive("ngSingleClick", ["$parse", function ($parse) {
             };
         }
     };
-} ]);
+}]);
